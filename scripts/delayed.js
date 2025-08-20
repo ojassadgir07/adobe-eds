c8649e35b3b54f09f69b0daaad799576b5ad3818
saif-pathan-ust-com <saif.pathan@ust.com>
2025-08-20 16:48:46 +0530

    EDS | test script execution

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,16 +1,10 @@
 // add delayed functionality here
-// (function () {
-//   const script = document.createElement("script");
-//   script.src = "https://www.google-analytics.com/analytics.js";
-//   script.defer = true; // same as <script defer>
-//   document.head.appendChild(script);
-// })();
 (function () {
   function loadScript(src) {
     return new Promise((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
-      script.defer = true; // makes it like <script defer>
+      script.async = true; // makes it like <script >
       script.onload = () => resolve(src + " loaded");
       script.onerror = () => reject(new Error("Failed to load " + src));
       document.head.appendChild(script);
@@ -19,9 +13,11 @@
 
   // Load multiple scripts
   Promise.all([
-    loadScript("https://www.google-analytics.com/analytics.js"),
-    loadScript("//assets.adobedtm.com/9c5fb1be77d6/aee11c13f5d9/launch-eb7b10085f33.min.js"),
-    loadScript("https://www.google.com/recaptcha/api.js")
+     setTimeout(() => {
+         loadScript("https://www.google-analytics.com/analytics.js"),
+         loadScript("//assets.adobedtm.com/9c5fb1be77d6/aee11c13f5d9/launch-eb7b10085f33.min.js"),
+         loadScript("https://www.google.com/recaptcha/api.js")
+     })
   ])
     .then(messages => {
       console.log("All scripts loaded:", messages);
